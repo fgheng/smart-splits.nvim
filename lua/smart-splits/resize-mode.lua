e8313ec0e0fb8407a3f6af96ba9d1242bdb2ef7e
forever <redusty@163.com>
2022-05-29 02:24:08 +0800

    fix the bug that nvim-tree can not resize

--- lua/smart-splits/resize-mode.lua
+++ lua/smart-splits/resize-mode.lua
@@ -5,7 +5,7 @@ local keymap_restore = {}
 local function smart_autocmd()
     local group_id = vim.api.nvim_create_augroup('smart-splits', { clear = true })
     -- vim.api.nvim_create_autocmd({ 'BufAdd', 'BufNew', 'BufNewFile' }, {
-    vim.api.nvim_create_autocmd({ 'BufWinEnter', 'BufAdd' }, {
+    vim.api.nvim_create_autocmd({ 'BufWinEnter', 'BufAdd', 'WinEnter' }, {
         pattern = "*",
         group = group_id,
         callback = function(buf)
